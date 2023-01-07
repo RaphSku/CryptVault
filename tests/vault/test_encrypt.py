9ee297237ccff3083667e2a630b258d50850944d
RaphSku <rapsku.dev@gmail.com>
2023-01-07 15:22:36 +0100

    Added tests for the vault package

--- tests/vault/test_encrypt.py
+++ tests/vault/test_encrypt.py
@@ -1,4 +1,30 @@
-import pytest
-
-from cryptvault.vault import Secret, EncryptedSecret
+from cryptvault.vault import Secret
 from cryptvault.vault import encrypt_secrets, decrypt_secret
+
+from ..fixtures import secrets, secret_key
+
+
+class TestEncryption:
+    def test_encryption_s01(self, secret_key: str, secrets: list[Secret]):
+        encrypted_secrets = encrypt_secrets(in_secrets = secrets, secret_key = secret_key)
+
+        is_bytes        = []
+        number_of_bytes = []
+        for encrypted_secret in encrypted_secrets:
+            is_bytes.append(isinstance(encrypted_secret.value, bytes))
+            number_of_bytes.append(len(encrypted_secret.value))
+
+        assert all(is_bytes) == True
+        assert number_of_bytes == [34, 34]
+
+
+    def test_decryption_s01(self, secret_key: str, secrets: list[Secret]):
+        encrypted_secrets = encrypt_secrets(in_secrets = secrets, secret_key = secret_key)
+
+        decrypted_secrets = []
+        for encrypted_secret in encrypted_secrets:
+            decrypted_secrets.append(decrypt_secret(secret = encrypted_secret, secret_key = secret_key))
+
+        for index in range(len(decrypted_secrets)):
+            assert decrypted_secrets[index].key == secrets[index].key
+            assert decrypted_secrets[index].value == secrets[index].value
--- tests/vault/test_registry.py
+++ tests/vault/test_registry.py
@@ -1,15 +1,84 @@
-import pytest
 import os
-import re
-import base64
-import pandas as pd
 import pyarrow.parquet as pq
 
 from cryptvault.vault import (
     Registry, 
+    RegisterManager,
     Secret, 
-    EncryptedSecret, 
-    encrypt_secrets, 
-    decrypt_secret
 )
+from ..fixtures import secrets, secret_key
 
+
+def cleanup():
+    base_path = os.path.expanduser("~/cryptvault")
+    os.remove(f"{base_path}/test_secrets.parquet")
+    os.remove(f"{base_path}/test.txt")
+    os.rmdir(f"{base_path}")
+
+
+class TestRegistry:
+    def test_registry_initialisation_s01(self, secret_key: str):
+        act_context = "test"
+        registry    = Registry(context = act_context, secret_key = secret_key)
+
+        assert registry.context == act_context
+
+
+    def test_store_secrets_s01(self, secret_key: str, secrets: list[Secret]):
+        registry  = Registry(context = "test", secret_key = secret_key)
+        registry.store_secrets(secrets = secrets)
+
+        df = pq.read_table("~/cryptvault/test_secrets.parquet").to_pandas()
+        
+        assert isinstance(df['key1'].value, bytes)
+        assert isinstance(df['key2'].value, bytes)
+        assert all(df.columns == ['key1', 'key2'])
+        assert df.index.values[0] == 'value'
+
+        cleanup()
+
+
+    def test_store_secrets_s02(self, secret_key: str, secrets: list[Secret]):
+        registry  = Registry(context = "test", secret_key = secret_key)
+        registry.store_secrets(secrets = secrets)
+
+        assert os.path.exists(f'{registry.base_path}/test.txt')
+
+        cleanup()
+
+
+class TestRegisterManager:
+    def test_register_manager_initialisation_s01(self, secret_key: str, secrets: list[Secret]):
+        registry  = Registry(context = "test", secret_key = secret_key)
+        registry.store_secrets(secrets = secrets)
+
+        register_manager = RegisterManager()
+        
+        assert register_manager.registries == {'test': f"{register_manager.base_path}/test_secrets.parquet"}
+
+        cleanup()
+
+
+    def test_register_manager_secret_key_retrieval_s01(self, secret_key: str, secrets: list[Secret]):
+        registry  = Registry(context = "test", secret_key = secret_key)
+        registry.store_secrets(secrets = secrets)
+
+        register_manager = RegisterManager()
+        secret_key = register_manager.get_secret_key(context = "test")
+
+        assert isinstance(secret_key, bytes)
+
+        cleanup()
+
+
+    def test_register_manager_encrypted_secret_retrieval_s01(self, secret_key: str, secrets: list[Secret]):
+        registry  = Registry(context = "test", secret_key = secret_key)
+        registry.store_secrets(secrets = secrets)
+
+        register_manager = RegisterManager()
+        encrypted_secret = register_manager.get_encrypted_secret(context = "test", key = "key1")
+
+        assert encrypted_secret.key == secrets[0].key
+        assert isinstance(encrypted_secret.value, bytes)
+
+        cleanup()
